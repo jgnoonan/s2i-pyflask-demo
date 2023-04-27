6eb64d12ec0ee04b805d81564134ced4730df1b2
Joseph G. Noonan <jgnoonan@comcast.net>
2023-04-27 17:05:20 -0400

    Set initial versions

--- app.py
+++ app.py
@@ -3,15 +3,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<h1>Hello, World ! - Pyflask Demo - Joe Noonan</h1>'
+    return '<h1>Hello, World ! - Pyflask Demo</h1>'
 
 @app.route('/version')
 def get_version():
-    return '<h1>App version : <b>3.0</b></h1>'
+    return '<h1>App version : <b>1.0</b></h1>'
 
 @app.route('/test')
 def get_test():
-    return '<h1>You are accessing /test endpoint v3.0</h1>'
+    return '<h1>You are accessing /test endpoint v1.0</h1>'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
